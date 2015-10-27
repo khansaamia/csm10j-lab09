4adfb506a9f829475b7010ee87eabac5dbcf7167
khansaamia <khansaamia@yahoo.com>
2015-10-27 16:19:49 -0700

    Create CarA.java

---
+++ src/Polymorphism/CarA.java
@@ -0,0 +1,13 @@
+package polymorphism;
+
+//public class carA that extends from the abstract class vehicle
+public class CarA extends Vehicle{
+
+    //constructor function
+	public CarA()
+	{
+            //call superclass constructor function
+		super();
+	}
+	
+}
